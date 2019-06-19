3051b4757845550241f232968d5f76083864008e
System Administrator <root@Hardiks-MAC.local>
2019-06-19 18:14:45 -0400

    Updated Package JSON

--- src/App.js
+++ src/App.js
@@ -40,12 +40,38 @@ class App extends Component {
       const cityList = [];
       for (let i = 0; i < response.data.length; i++) {
         var cityArray = [];
-        cityArray["key"] = cityArray["value"] = response.data[i].city;
+        cityArray["key"] = cityArray["value"] = this.replaceDiacritics(
+          response.data[i].city
+        );
         cityList.push(cityArray);
       }
 
       this.setState({ cityList });
     });
+  }
+  replaceDiacritics(str) {
+    var diacritics = [
+      { char: "A", base: /[\300-\306]/g },
+      { char: "a", base: /[\340-\346]/g },
+      { char: "E", base: /[\310-\313]/g },
+      { char: "e", base: /[\350-\353]/g },
+      { char: "I", base: /[\314-\317]/g },
+      { char: "i", base: /[\354-\357]/g },
+      { char: "O", base: /[\322-\330]/g },
+      { char: "o", base: /[\362-\370]/g },
+      { char: "U", base: /[\331-\334]/g },
+      { char: "u", base: /[\371-\374]/g },
+      { char: "N", base: /[\321]/g },
+      { char: "n", base: /[\361]/g },
+      { char: "C", base: /[\307]/g },
+      { char: "c", base: /[\347]/g }
+    ];
+
+    diacritics.forEach(function(letter) {
+      str = str.replace(letter.base, letter.char);
+    });
+
+    return str;
   }
 
   onPageChanged = data => {
